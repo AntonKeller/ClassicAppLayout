feeca481d76f0b21290ed2af02d1131b63bf0a2f
Anton <ToniWebDev@mail.ru>
2021-07-30 21:27:51 +0300

    refactoring project, styles header.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Header from "./components/Header/Header";
+// import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      {/*<NavBar/>*/}
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,10 @@
 import React from 'react'
-import styles from './Header.module.css'
-// import {Link} from 'react-router-dom'
+import styles from './styles/Header.module.css'
 
-const links = [
-    'home',
-    'how it works',
-    'discover a project',
-    'blog',
-    'find out more'
-]
-
-export default function Header() {
+function Header() {
     return(
         <header className={styles.header}>
-            <div className={styles.logo}></div>
-            <div className={styles.nav}>
-                {
-                    links.map(item =>{
-                        return(
-                            <a key={item} href="#" className={styles.nav__link}>{item}</a>
-                        )
-                    })
-                }
-                {/*<a href="#" className={styles.nav__link}>home</a>*/}
-                {/*<a href="#" className={styles.nav__link}>how it works</a>*/}
-                {/*<a href="#" className={styles.nav__link}>discover a project</a>*/}
-                {/*<a href="#" className={styles.nav__link}>blog</a>*/}
-                {/*<a href="#" className={styles.nav__link}>find out more</a>*/}
-            </div>
-            <div className={styles.login}>Login</div>
+
         </header>
     )
 }
