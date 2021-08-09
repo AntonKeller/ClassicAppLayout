43624a08b0367fe93a8690967400db5da5a72428
Anton <ToniWebDev@mail.ru>
2021-08-09 21:40:31 +0300

    refactor Banner styles, add adaptive

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className={styles.app}>
       <Header />
-      {/*<Banner />*/}
+      <Banner />
       {/*<Content />*/}
       {/*<FindMenu />*/}
       {/*<TicketsBlock />*/}
